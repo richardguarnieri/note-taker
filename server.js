7a9e97a3811c845096a3d6ee6f02d3de5d08b257
Richard Guarnieri <richard.gm@outlook.com>
2022-05-29 14:06:59 -0400

    refactored to synchronous model

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const path = require('path');
-const fs = require('node:fs/promises');
+const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
-// Import DB JSON
-const dbNotesPath = './db/db2.json';
-const dbNotes = require('./db/db2.json');
+// Path to the JSON "database"
+const dbPath = './db/db2.json';
 
 // Start Express Web Server Application
 const app = express()
@@ -23,48 +22,22 @@ app.get('/notes', (req, res) => {
 
 // GET /api/notes should read the db.json file and return all saved notes as JSON
 app.get('/api/notes', (req, res) => {
-    res.status(200).json(dbNotes);
+    const data = JSON.parse(fs.readFileSync(dbPath, 'utf8'))
+    res.status(200).json(data);
 })
 
 // POST /api/notes should receive a new note to save on the request 
 // body, add it to the db.json file, and then return the new note 
 // to the client
-
-const writeFn = async (destination, content) => {
-    try {
-        await fs.writeFile(destination, JSON.stringify(content))
-        console.log(`Data written to ${destination}`);
-    } catch (err) {
-        console.log(err);
-    }
-};
-
- const readAndWriteFn = async (filePath, data) => {
-    try {
-        const content = await fs.readFile(filePath, 'utf8');
-        const contentParsed = JSON.parse(content);
-        contentParsed.push(data);
-        writeFn(filePath, contentParsed);
-    } catch (err) {
-        console.log(err);
-    }
-};
-
-
-
 app.post('/api/notes', (req, res) => {
-    const { title, text } = req.body;
-    const note = {
-        title,
-        text,
-        id: uuidv4()
-    }
-    readAndWriteFn(dbNotesPath, note);
-    res.status(200).send();
-
-    
+    const note = req.body;
+    note.id = uuidv4();
+    const data = JSON.parse(fs.readFileSync(dbPath, 'utf8'))
+    data.push(note);
+    fs.writeFileSync(dbPath, JSON.stringify(data));
+    console.log(`${JSON.stringify(note)} has been sucessfully added to JSON file`);
+    res.status(200).json(data);
 });
-
 
 // GET * should return the index.html file
 app.get('*', (req, res) => {
@@ -72,5 +45,5 @@ app.get('*', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Express Web Server Listening on localhost:${port}`);
+    console.log(`Server listening on http://localhost:${port}`);
 })
