27566bda6794c632c028bc6ce77f98f773675353
Richard Guarnieri <richard.gm@outlook.com>
2022-05-29 09:30:37 -0400

    get /notes route completed

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const fs = require('fs');
 
 // Start Express Web Server Application
@@ -6,8 +7,11 @@ const app = express()
 const port = 3001;
 
 // Set up middleware stack
-app.use(express.static('/public'));
+app.use(express.static('./public'));
 
+app.get('/notes', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, 'public', 'notes.html'));
+})
 
 app.listen(port, () => {
     console.log(`Express Web Server Listening on localhost:${port}`);
